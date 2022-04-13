042bc53f5f5c7ad437cca8c82461f09ff901c2a2
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2022-04-13 11:20:02 +0200

    fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route path="/" element={<App />} />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')

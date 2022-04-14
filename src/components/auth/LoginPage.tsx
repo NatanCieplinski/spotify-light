7670a9012e3d95e857bc7eab27f844f20b9ea916
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2022-04-14 14:25:10 +0200

    add spotify authentication using implicit grant

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -1,3 +1,19 @@
+import queryString from 'query-string'
+
 export const LoginPage: React.FC = () => {
-  return <div>Login</div>
+  const url =
+    'https://accounts.spotify.com/authorize?' +
+    queryString.stringify({
+      client_id: '7b9179880e2746d5954a44f406ea647f',
+      scope:
+        'user-read-private user-read-email playlist-read-private user-modify-playback-state user-read-playback-state user-read-currently-playing app-remote-control',
+      response_type: 'token',
+      redirect_uri: 'http://localhost:3000/',
+    })
+
+  return (
+    <div>
+      <a href={url}>Login</a>
+    </div>
+  )
 }
